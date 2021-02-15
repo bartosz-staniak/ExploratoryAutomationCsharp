9c3f2bb703a13eaf1ee81d7cc6f81fb65457f729
bs <bartosz.staniak@gmail.com>
2021-02-15 22:36:15 +0100

    added a new test and page representation

---
+++ ElementsMethods.cs
@@ -0,0 +1,62 @@
+// using MbUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PageObjectPatternDemo
+{
+    [TestClass]
+
+    public class InitialPage
+    {
+        public IWebDriver webDriver; // there wasn't "public"
+        public InitialPage(IWebDriver driver)
+        {
+            webDriver = driver; // there was "this."webDriver
+            this.hideImage();
+            this.WaitTenSec();
+        }
+
+
+        public static By buttonImage = By.Id("buttonImage");
+
+        public void hideImage()
+        {
+            webDriver.FindElement(InitialPage.buttonImage).Click();
+            WaitTenSec();
+        }
+
+
+        public void WaitTenSec()
+        {
+            WebDriverWait webDriverWait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+        }
+
+    }
+
+
+    public class ResultsPage : InitialPage
+    {
+        public ResultsPage(IWebDriver driver) : base(driver)
+        {
+            this.sort();
+            this.WaitTenSec();
+        }
+
+        public static By sortProducts = By.Id("IdGoesHere");
+
+        public void sort()
+        {
+            webDriver.FindElement(ResultsPage.sortProducts).Click();
+            WaitTenSec();
+        }
+
+    }
+
+}
---
+++ ImageButtonTest.cs
@@ -0,0 +1,41 @@
+﻿// using MbUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace PageObjectPatternDemo
+{
+    [TestClass]
+    public class ImmageButtonTest
+    {
+        IJavaScriptExecutor buttonexec;
+
+        IWebDriver webDriver;
+
+        public ImmageButtonTest()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("disable-infobars");
+            webDriver = new ChromeDriver(@"C:\ChromeDriver", chromeOptions);
+            webDriver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
+            webDriver.Manage().Window.Maximize();
+            WebDriverWait webDriverWait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(50));
+        }
+
+        [TestMethod]
+        public void FirstTest()
+        {
+            InitialPage initialPage = new InitialPage(webDriver);
+
+
+            string buttonImageText = webDriver.FindElement(InitialPage.buttonImage).Text;
+            Assert.AreEqual("Show this image", buttonImageText);
+
+
+            webDriver.Quit();
+        }
+    }
+
+}
