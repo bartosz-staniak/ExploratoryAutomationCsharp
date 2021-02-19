1dfc5d91d4b4d7087cc56dc0e89545e7ad5be668
bs <bartosz.staniak@gmail.com>
2021-02-19 02:11:57 +0100

    undid last change

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -87,10 +87,6 @@ namespace PageObjectPatternDemo
 
             System.Threading.Thread.Sleep(5000);
 
-            action.ContextClick().Build().Perform();
-
-            System.Threading.Thread.Sleep(5000);
-
             // webDriver.FindElement(dataList).Click();
 
             // Actions action = new Actions(webDriver);
