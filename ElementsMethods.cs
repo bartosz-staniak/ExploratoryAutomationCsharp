42493c01a513f01878e24a484556a0416f1d1fd4
bs <bartosz.staniak@gmail.com>
2021-02-25 02:15:30 +0100

    completed the action

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -76,7 +76,7 @@ namespace PageObjectPatternDemo
             string opinionOneToString = webDriver.FindElement(opinionOne).GetAttribute("value");
             webDriver.FindElement(dataList).SendKeys(opinionOneToString);
             Actions action = new Actions(webDriver);
-            action.MoveToElement(webDriver.FindElement(dataList));
+            action.MoveToElement(webDriver.FindElement(dataList)).MoveByOffset(0, 100).Build().Perform();
             
             
             /* System.Threading.Thread.Sleep(5000);
