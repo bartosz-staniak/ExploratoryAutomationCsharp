34ca1c0e9acfb9fbc02d10eae8c846c0fafdc1a3
bs <bartosz.staniak@gmail.com>
2021-02-27 15:12:50 +0100

    included some libraries

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -1,6 +1,9 @@
 ﻿// using MbUnit.Framework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace PageObjectPatternDemo
 {
