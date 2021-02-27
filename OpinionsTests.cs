e7e4a5a831b2a48a684d327e1d8be5da8869c20e
bs <bartosz.staniak@gmail.com>
2021-02-27 17:16:33 +0100

    dissabled IWebDriver with a comment

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -7,7 +7,7 @@ namespace PageObjectPatternDemo
     [TestClass]
     public class OpinionsTests : ImageButtonTest
     {
-        IWebDriver webDriver; // trying to find out if this solves the inaccessibility due to protection level issue
+        // IWebDriver webDriver; // trying to find out if this solves the inaccessibility due to protection level issue
 
         /*
         public OpinionsTests() : base()
