0d2a81176c0d3b8b0ccdba83c9c39857eeec3ebc
bs <bartosz.staniak@gmail.com>
2021-02-27 17:44:53 +0100

    New file with an old class

---
+++ Configuration.cs
@@ -0,0 +1,28 @@
+﻿// using MbUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace PageObjectPatternDemo
+{
+    [TestClass]
+    public class Configuration
+    {
+        public IWebDriver webDriver;
+
+
+        public Configuration()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("disable-infobars");
+            webDriver = new ChromeDriver(@"C:\ChromeDriver", chromeOptions);
+            webDriver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
+            webDriver.Manage().Window.Maximize();
+            WebDriverWait webDriverWait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(50));
+        }
+
+    }
+
+}
