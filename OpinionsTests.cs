7806e6d64c64b9cc7cbbab0d5f56c0b500f56bad
bs <bartosz.staniak@gmail.com>
2021-02-27 14:29:15 +0100

    changed the name of the method by adding Overwrite to its name

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -15,7 +15,7 @@ namespace PageObjectPatternDemo
         }
 
         [TestMethod]
-        public void DataListTest()  // doesn't work due to: "Object reference not set to an instance of an object."
+        public void DataListTestOverwrite()  // doesn't work due to: "Object reference not set to an instance of an object."
         {
             Datalist dataList = new Datalist(webDriver);
 
