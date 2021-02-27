34f16aed191dc6b648a88d21ae8bc50033129312
bs <bartosz.staniak@gmail.com>
2021-02-27 16:48:15 +0100

    removed unused libraries

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -1,9 +1,6 @@
 ﻿// using MbUnit.Framework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
 
 namespace PageObjectPatternDemo
 {
