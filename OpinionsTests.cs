92f6cd1be3abf3546951012d499451c37ac0eae7
bs <bartosz.staniak@gmail.com>
2021-02-27 14:03:13 +0100

    made a new file out of OpinionsTests class

---
+++ OpinionsTests.cs
@@ -0,0 +1,19 @@
+﻿// using MbUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace PageObjectPatternDemo
+{
+    [TestClass]
+    public class OpinionsTests : ImageButtonTest
+    {
+        IWebDriver webDriver; // trying to find out if this solves the inaccessibility due to protection level issue
+
+        public OpinionsTests() : base()
+        {
+
+        }
+
+    }
+
+}
