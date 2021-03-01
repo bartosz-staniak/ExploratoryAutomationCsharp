230a7f9eac69ed93bf23bbe1a0a5d5c050030b88
bs <bartosz.staniak@gmail.com>
2021-03-01 22:08:53 +0100

    inverted the boolean assert in order to find out if the test will fail

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -19,7 +19,7 @@ namespace PageObjectPatternDemo
         {
             Opinions multipleOpinionsTest = new Opinions(webDriver, 3, test);
 
-            Assert.IsFalse(webDriver.FindElement(Datalist.hideousButton).Displayed);
+            Assert.IsTrue(webDriver.FindElement(Datalist.hideousButton).Displayed);
 
             webDriver.Quit();
         }
