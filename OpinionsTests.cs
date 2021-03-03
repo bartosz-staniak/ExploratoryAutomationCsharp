916b5209839fcdd1363c57d9117abc136c2018cd
bs <bartosz.staniak@gmail.com>
2021-03-03 03:16:48 +0100

    disabled the code populating the textarea

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -93,7 +93,7 @@ namespace PageObjectPatternDemo
             {
                 // TestingTheNewOpinoinsConstructor13(); // this one is failing, most probably due to driver.Quit() method
 
-                Reporting reporting = new Reporting(webDriver);
+                // Reporting reporting = new Reporting(webDriver);
                 Assert.IsTrue(webDriver.FindElement(Reporting.textArea).Displayed);
                 Assert.IsNotNull(webDriver.FindElement(Reporting.textArea).Text);
 
