3c16ce58bfe6dfb357807cfc15a0fc1707a8d746
bs <bartosz.staniak@gmail.com>
2021-03-03 03:19:20 +0100

    attempting to trigger an error

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -94,8 +94,8 @@ namespace PageObjectPatternDemo
                 // TestingTheNewOpinoinsConstructor13(); // this one is failing, most probably due to driver.Quit() method
 
                 // Reporting reporting = new Reporting(webDriver);
-                // Assert.IsTrue(webDriver.FindElement(Reporting.textArea).Displayed);
-                Assert.IsNotNull(webDriver.FindElement(Reporting.textArea).Text);
+                Assert.IsFalse(webDriver.FindElement(Reporting.textArea).Displayed);
+                // Assert.IsNotNull(webDriver.FindElement(Reporting.textArea).Text);
 
             } finally {
                 System.Threading.Thread.Sleep(5000);
