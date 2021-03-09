8e8797879a7397e3dc5ea408be5ff322d909d477
bs <bartosz.staniak@gmail.com>
2021-03-09 03:41:26 +0100

    added a commment that the assert doesn't work

--- OpinionsTests.cs
+++ OpinionsTests.cs
@@ -116,7 +116,7 @@ namespace PageObjectPatternDemo
             try
             {
                 // Submitting submitting = new Submitting(webDriver);
-                Assert.IsNotNull(webDriver.FindElement(Submitting.readReport).Text);
+                Assert.(webDriver.FindElement(Submitting.readReport).Text); // it doesn't work
             } finally {
                 System.Threading.Thread.Sleep(5000);
                 webDriver.Quit();
