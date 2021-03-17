25d8e43e92eb193edf93ed679d6d06556af7e137
bs <bartosz.staniak@gmail.com>
2021-03-17 04:10:10 +0100

    added an incorrect assert; it should fail

--- ImageButtonTest.cs
+++ ImageButtonTest.cs
@@ -72,6 +72,7 @@ namespace PageObjectPatternDemo
 
                 initialPage.hideImageBoolQuitAssert(webDriver, true, false);
                 initialPage.hideImageBoolQuitAssert(webDriver, true, true);
+                Assert.AreEqual("Hide this imag", webDriver.FindElement(InitialPage.buttonImage).Text);
             } finally {
                 webDriver.Quit();
             }
