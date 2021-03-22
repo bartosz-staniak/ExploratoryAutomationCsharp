c39d7204ab5b3228ab310d7bfc8b4dfe150da0a5
bs <bartosz.staniak@gmail.com>
2021-03-22 21:40:29 +0100

    removed redundant element

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -100,11 +100,11 @@ namespace PageObjectPatternDemo
             this.WaitTenSec();
         }
 
-        public static By button1 = By.Id("button1");
+        public static By hideousButton = By.Id("button1");
 
         public void hideMainDiv()
         {
-            webDriver.FindElement(InitialPageII.button1).Click();
+            webDriver.FindElement(InitialPageII.hideousButton).Click();
             WaitTenSec();
         }
 
@@ -138,7 +138,7 @@ namespace PageObjectPatternDemo
         public static By opinionEleven = By.XPath("//*[@id='opinions']/option[11]");
         public static By opinionTwelve = By.XPath("//*[@id='opinions']/option[12]");
         public static By opinionThirteen = By.XPath("//*[@id='opinions']/option[13]");
-        public static By hideousButton = By.XPath("/html/body/div/button[2]");
+        // public static By hideousButton = By.XPath("/html/body/div/button[2]");
 
         public void navigateDataList()
         {
