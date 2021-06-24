e6ddf502763c24f67ec1d5b312275c7275e6b4c3
bs <bartosz.staniak@gmail.com>
2021-06-25 00:43:40 +0200

    copied a method and changed its name

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -58,5 +58,22 @@ namespace PageObjectPatternDemo
 
         }
 
+        [TestMethod]
+        public void getNoRecordTest()
+        {
+            try
+            {
+                API_elements api_Elements = new API_elements(webDriver);
+                api_Elements.getRecordFromDb(1);
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TextToBePresentInElement(webDriver.FindElement(API_elements.readRecordParagraph), "Id:")); //returned failed result with "Ib:"
+                Assert.IsTrue(webDriver.FindElement(API_elements.readRecordParagraph).Text.Contains("Id:"));
+            }
+            finally
+            {
+                webDriver.Quit();
+            }
+
+        }
+
     }
 }
