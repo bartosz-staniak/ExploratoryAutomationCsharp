80b60a45139bcf1bcf8b8188d0df38f5671a7b9c
bs <bartosz.staniak@gmail.com>
2021-07-09 11:18:51 +0200

    added the public keyword

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -105,7 +105,7 @@ namespace PageObjectPatternDemo
 
         }
 
-        IList<IWebElement> redundants = (IList<IWebElement>)By.ClassName("redundant");
+        public IList<IWebElement> redundants = (IList<IWebElement>)By.ClassName("redundant");
     }
 
     public class Datalist : InitialClass
