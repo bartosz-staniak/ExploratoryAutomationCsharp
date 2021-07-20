a8d50ed8b602722a0708fff80db1ca4a730034e9
bs <bartosz.staniak@gmail.com>
2021-07-21 00:17:29 +0200

    changed method's name to reflect the current state better

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -77,7 +77,7 @@ namespace PageObjectPatternDemo
         }
 
         [TestMethod]
-        public void confirmRedundantElementsPresenceTest()
+        public void NegativeRedundantElementsPresenceTest()
         {
             try
             {
