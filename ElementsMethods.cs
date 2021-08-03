59b6e7354e11349649fa40312fbb184d8530141d
bs <bartosz.staniak@gmail.com>
2021-08-03 03:02:56 +0200

    added a new class for locating defunct elements

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -114,6 +114,15 @@ namespace PageObjectPatternDemo
         }*/
     }
 
+    public class DefunctElements : InitialClass //perhaps it would be better to create a separate class from which Defunct and Redundant classes would inherit
+    {
+        public DefunctElements(IWebDriver driver) : base(driver)
+        {
+
+        }
+        public static IReadOnlyCollection<IWebElement> defunct = webDriver.FindElements(By.ClassName("defunct"));
+    }
+
     public class Datalist : InitialClass
     {
         public Datalist(IWebDriver driver) : base (driver)
