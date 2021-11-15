745a862a0ba435e15e85b8dccd3e5fd56eea6d2c
bs <bartosz.staniak@gmail.com>
2021-11-15 23:17:11 +0100

    updated the method used with a required parameter

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -103,7 +103,7 @@ namespace PageObjectPatternDemo
                 System.Threading.Thread.Sleep(5000);
                 Assert.IsTrue(AlertButton.IsAlertPresent(webDriver));
 
-                AlertButton.SwitchToAlert();
+                AlertButton.SwitchToAlert(webDriver);
             }
             finally
             {
