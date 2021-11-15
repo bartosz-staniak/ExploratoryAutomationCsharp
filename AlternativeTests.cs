34a861a28d2d3a42c835c461e57bf8b354364830
bs <bartosz.staniak@gmail.com>
2021-11-15 10:50:51 +0100

    inverted the assert in order to rule out a false negative

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -101,7 +101,7 @@ namespace PageObjectPatternDemo
             {
                 AlertButton.TriggerAlert(webDriver);
                 System.Threading.Thread.Sleep(5000);
-                Assert.IsTrue(AlertButton.IsAlertPresent(webDriver));
+                Assert.IsFalse(AlertButton.IsAlertPresent(webDriver));
             }
             finally
             {
