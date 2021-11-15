be0bb65da9735f6d0ad8f521abfb49807564ed47
bs <bartosz.staniak@gmail.com>
2021-11-15 03:25:48 +0100

    removed an obsolete piece of code

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -111,7 +111,7 @@ namespace PageObjectPatternDemo
         {
             Boolean returnedValue = false;
 
-            IAlert alertPresent = ExpectedConditions.AlertIsPresent().Invoke(webDriver);
+            IAlert alertPresent;
 
             return alertPresent;
         }
