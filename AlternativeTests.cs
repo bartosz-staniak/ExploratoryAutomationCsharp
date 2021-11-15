a93904dae1ba8c156284360214ec7c4d83ea0b1d
bs <bartosz.staniak@gmail.com>
2021-11-15 06:09:13 +0100

    passed the required parameter

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -99,7 +99,7 @@ namespace PageObjectPatternDemo
         {
             try
             {
-                AlertButton.TriggerAlert();
+                AlertButton.TriggerAlert(webDriver);
             }
             finally
             {
