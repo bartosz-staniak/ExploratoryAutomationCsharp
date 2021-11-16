f241274494ae178b7506adf45535d6bdd168e9a3
bs <bartosz.staniak@gmail.com>
2021-11-16 05:08:10 +0100

    intorduced an incorrect text in order to rule out a false negative

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -103,7 +103,7 @@ namespace PageObjectPatternDemo
                 System.Threading.Thread.Sleep(5000);
                 Assert.IsTrue(AlertButton.IsAlertPresent(webDriver));
 
-                AlertButton.DismissAlert(webDriver, "A test alert");
+                AlertButton.DismissAlert(webDriver, "A test aler");
             }
             finally
             {
