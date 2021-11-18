735f212d74c8f410c451caa447939e41f5a615c4
bs <bartosz.staniak@gmail.com>
2021-11-18 17:41:16 +0100

    updated method's name

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -103,7 +103,7 @@ namespace PageObjectPatternDemo
                 System.Threading.Thread.Sleep(5000);
                 Assert.IsTrue(AlertButton.IsAlertPresent(webDriver));
 
-                AlertButton.DismissAlert(webDriver, "A test alert");
+                AlertButton.VerifyAlertPresence(webDriver, "A test alert");
             }
             finally
             {
