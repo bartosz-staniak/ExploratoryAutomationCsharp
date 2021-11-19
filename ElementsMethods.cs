02528bb6a1eb9c89f85ea575c3d142eb96818743
bs <bartosz.staniak@gmail.com>
2021-11-19 07:03:32 +0100

    removed the static modifier from the variable

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -100,7 +100,7 @@ namespace PageObjectPatternDemo
 
     public class PopUpButtonClass : InitialClass
     {
-        static String _element;
+        String _element;
 
         public PopUpButtonClass(IWebDriver driver, String element) : base(driver)
         {
