d96f0bf782c5ab9aa031eb63579fc4e4331ac453
bs <bartosz.staniak@gmail.com>
2021-11-25 00:25:55 +0100

    reverted to the parameter and updated the comment

--- ElementsMethods.cs
+++ ElementsMethods.cs
@@ -138,7 +138,7 @@ namespace PageObjectPatternDemo
         {
             IAlert alert = webDriver.SwitchTo().Alert();
             Assert.AreEqual(alert.Text, testedPopUpTextSTring);
-            alert.SendKeys("blah"); // does not work
+            alert.SendKeys(inputString); // does not work with a hardcoded string either
         }
 
         public static void AcceptPopUp(IWebDriver webDriver)
