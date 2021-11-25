819ada33abebd27319fa9901f1256fdb7311c62a
bs <bartosz.staniak@gmail.com>
2021-11-26 00:05:35 +0100

    removed some redundant comments

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -173,8 +173,6 @@ namespace PageObjectPatternDemo
             {
                 PopUpMethodsClass popUpMethodsClass = new PopUpMethodsClass(webDriver, "promptButton");
                 popUpMethodsClass.TriggerPopUp(webDriver);
-                // PopUpMethodsClass.VerifyPopUpPresence(webDriver, "A test prompt");
-                // PopUpMethodsClass.PopulatePromptInput(webDriver, "A test input string"); // does not work
                 PopUpMethodsClass.VerifyPopUpSendKeysAccept(webDriver, "A test prompt", "A test input string");
                 System.Threading.Thread.Sleep(5000);
             }
