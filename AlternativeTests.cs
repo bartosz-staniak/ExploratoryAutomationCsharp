df85947a9b6b6d6db57579978b469ac574495bdb
bs <bartosz.staniak@gmail.com>
2021-11-26 00:18:01 +0100

    substitued the hardcoded parameter with the new variable

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -174,7 +174,7 @@ namespace PageObjectPatternDemo
                 string triggerElementId = "promptButton";
                 string testedInputString = "A test input string";
                 string testedPopUpText = "A test prompt";
-                PopUpMethodsClass popUpMethodsClass = new PopUpMethodsClass(webDriver, "promptButton");
+                PopUpMethodsClass popUpMethodsClass = new PopUpMethodsClass(webDriver, triggerElementId);
                 popUpMethodsClass.TriggerPopUp(webDriver);
                 PopUpMethodsClass.VerifyPopUpSendKeysAccept(webDriver, testedPopUpText, testedInputString);
                 System.Threading.Thread.Sleep(5000);
