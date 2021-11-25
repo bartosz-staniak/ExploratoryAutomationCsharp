14013001f7b65aa7cea351283c85bc1642db46bf
bs <bartosz.staniak@gmail.com>
2021-11-26 00:21:32 +0100

    called a method which also contains an assert

--- AlternativeTests.cs
+++ AlternativeTests.cs
@@ -178,6 +178,7 @@ namespace PageObjectPatternDemo
                 PopUpMethodsClass popUpMethodsClass = new PopUpMethodsClass(webDriver, triggerElementId);
                 popUpMethodsClass.TriggerPopUp(webDriver);
                 PopUpMethodsClass.VerifyPopUpSendKeysAccept(webDriver, testedPopUpText, testedInputString);
+                PopUpMethodsClass.VerifyPopUpPresence(webDriver, testedInputString);
                 System.Threading.Thread.Sleep(5000);
             }
             finally
