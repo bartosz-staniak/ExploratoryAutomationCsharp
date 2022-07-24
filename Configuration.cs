a4dfd89043f93022ca6c5cc2624633e2d8f5498a
bs <bartosz.staniak@gmail.com>
2022-07-25 01:44:13 +0200

    removed exception handling as the report file has been located and functional

--- Configuration.cs
+++ Configuration.cs
@@ -27,8 +27,7 @@ namespace PageObjectPatternDemo
                 webDriver.Manage().Window.Maximize();
                 wait = new WebDriverWait(webDriver, new TimeSpan(0, 0, 5));
                 createReportFile(); // part of the attempt to create the report file for the global use
-            } catch (Exception e){
-                Console.WriteLine("" + e);
+            } catch {
                 webDriver.Quit(); // if tests fail here then check driver version
             }
             
